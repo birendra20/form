33f4e59bfb368041d6a80713ea29d4cc7bd7bbad
birendra gupta <96100549+birendra20@users.noreply.github.com>
2022-03-22 20:29:20 +0530

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,10 @@
+import Userform from "./components/Userform";
+import "./styles.css";
+
+export default function App() {
+  return (
+    <div className="App">
+      <Userform />
+    </div>
+  );
+}
